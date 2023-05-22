bc9823d74e417203918ddabece28b55e013fda48
tesandev <tesan.dev@gmail.com>
2023-05-22 16:59:57 +0700

    Fixing Setting Halaman

--- app/src/main/java/com/tesan/gerejayohanes/fragment/SettingFragment.kt
+++ app/src/main/java/com/tesan/gerejayohanes/fragment/SettingFragment.kt
@@ -3,11 +3,14 @@ package com.tesan.gerejayohanes.fragment
 import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
+import android.transition.AutoTransition
+import android.transition.TransitionManager
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.tesan.gerejayohanes.R
 import com.tesan.gerejayohanes.api.ApiConfig
@@ -15,24 +18,25 @@ import com.tesan.gerejayohanes.databinding.FragmentSettingBinding
 import com.tesan.gerejayohanes.helper.Constant
 import com.tesan.gerejayohanes.helper.SharedPref
 import com.tesan.gerejayohanes.register.ResponseRegister
-import kotlinx.android.synthetic.main.activity_home.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class SettingFragment:Fragment() {
     private lateinit var binding: FragmentSettingBinding
-    lateinit var sharePref: SharedPref
+    private lateinit var sharePref: SharedPref
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSettingBinding.inflate(inflater, container, false)
 
+        sharePref = context?.let { SharedPref(it) }!!
+
         val idUsr = sharePref.getString(Constant.PREF_ID_USER)
         val nama = sharePref.getString(Constant.PREF_NAMA)
         val email = sharePref.getString(Constant.PREF_EMAIL)
-        val passwordsekarang = binding.edtPasswordCurrentProfile.editText?.text
+        val passwordlama = binding.edtPasswordLama.editText?.text
         val passwordbaru = binding.edtPasswordNewProfile.editText?.text
 
         binding.profilname.text = nama
@@ -42,17 +46,44 @@ class SettingFragment:Fragment() {
         binding.edtEmailProfile.editText?.setText(email)
 
         binding.btnUpdateProfile.setOnClickListener {
-            updateProfile(idUsr.toString(),nama.toString(),email.toString(),passwordsekarang.toString(),passwordbaru.toString())
+            updateProfile(idUsr.toString(),nama.toString(),email.toString(),passwordlama.toString(),passwordbaru.toString())
         }
+
+        binding.profilMore.setOnClickListener {
+            if(binding.editProfileDiv.isVisible == false){
+                TransitionManager.beginDelayedTransition(binding.headSetting, AutoTransition())
+                binding.editProfileDiv.setVisibility(View.VISIBLE)
+                binding.profilMore.setImageResource(R.drawable.ic_baseline_expand_less_24)
+            }else{
+                TransitionManager.beginDelayedTransition(binding.headSetting, AutoTransition())
+                binding.editProfileDiv.setVisibility(View.GONE)
+                binding.profilMore.setImageResource(R.drawable.ic_baseline_expand_more_24)
+            }
+        }
+
         return binding.root
 
     }
 
     private fun updateProfile(iduser:String,nama:String,email:String,passwordsekarang:String,passwordbaru:String) {
+        if (binding.edtPasswordLama.editText?.text.toString().isEmpty()){
+            binding.edtPasswordLama.error = "Password Sekarang Kosong"
+            binding.edtPasswordLama.requestFocus()
+            return
+        }
+
+        if (binding.edtPasswordNewProfile.editText?.text.toString().isEmpty()){
+            binding.edtPasswordNewProfile.error = "Password Baru Kosong"
+            binding.edtPasswordNewProfile.requestFocus()
+            return
+        }
+
         val loading = ProgressDialog(context)
         loading.setMessage("Loading...")
         loading.setCancelable(false)
         loading.show()
+
+        Log.e("profile",iduser+", "+nama+", "+email+", "+passwordsekarang+", "+passwordbaru)
 
         ApiConfig.instance.usermobileUpdate(iduser,nama,email,passwordsekarang,passwordbaru).enqueue(object : Callback<ResponseRegister>{
             override fun onResponse(
